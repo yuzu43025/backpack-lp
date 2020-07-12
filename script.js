860b84386f898052260194518ab2b02deb161ce8
yuzu43025 <66229014+yuzu43025@users.noreply.github.com>
2020-07-12 10:43:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(function(){
-$('#imgbox_fadein').hide().fadeIn(3000);
-$('.top__text_opacity').hide().fadeIn(3000);
-
+$('.top__text_opacity').hide().fadeIn(4500);
+  function animation(){
+      $('#imgbox_fadein').each(function(){
+        //ターゲットの位置を取得
+        var target = $(this).offset().top;
+        //スクロール量を取得
+        var scroll = $(window).scrollTop();
+        //ウィンドウの高さを取得
+        var windowHeight = $(window).height();
+        //ターゲットまでスクロールするとフェードインする
+        if (scroll > target - windowHeight){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+        }
+      });
+    }
+    animation();
+    $(window).scroll(function (){
+      animation();
+    });
 });
